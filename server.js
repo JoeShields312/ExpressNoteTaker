de868778acbed20f68fba2251b955fc2683b21b7
Joe Shields <shields.john.joseph@gmail.com>
2020-01-21 22:17:40 -0600

    removed nodemon

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const htmlRoutes = require("./routes/htmlRoutes");
-const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes.js");
+const apiRoutes = require("./routes/apiRoutes.js");
 const PORT = process.env.PORT || 3030;
 
 app.use(bodyParser.json());
